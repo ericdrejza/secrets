3b8215b8f6a25dc449e9bf1aaa0b66be11f515f0
Eric Drejza <edrejza@udel.edu>
2022-12-04 20:02:02 -0500

    Level 1 - Username and Password only

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,9 +9,28 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+};
+
+const User = new mongoose.model("user", userSchema);
+
 
 app.get("/", function(req, res) {
   res.render("home");
+});
+
+app.get('/logout', function(req, res) {
+  res.redirect('/');
 });
 
 app.get("/login", function (req, res) {
@@ -19,6 +39,42 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('secrets');
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser) {
+    if (err) { // If there's an error
+      console.log(err);
+    } else if (foundUser) { // If we found a user with that username
+      if (foundUser.password === password) { // If the passwords match
+        res.render('secrets');
+      } else {
+        console.log("Password doesn't match");
+        res.redirect('/login');
+      }
+    } else {
+      console.log("No user with that username found");
+      res.redirect('/login');
+    }
+  });
 });
 
 
